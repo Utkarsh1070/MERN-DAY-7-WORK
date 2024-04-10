c9fed275611949623c84fcabf5bc88f263721b62
Utkarsh1070 <150757318+Utkarsh1070@users.noreply.github.com>
2024-04-10 15:26:42 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Languages from './component/Languages'
+import Message from './component/Message'
+
+function App() {
+  return (
+    <>
+    <Message />
+    <Languages/>
+    </>
+  )
+}
+export default App
